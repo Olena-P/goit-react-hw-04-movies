655eb7825d23bd0027f037b900fb025461141f74
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-07-28 19:34:53 +0300

    card

--- src/Components/PageHeading/PageHeading.js
+++ src/Components/PageHeading/PageHeading.js
@@ -2,10 +2,6 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import styles from "./PageHeading.module.css";
 
-// export default function PageHeading({ text }) {
-//   return <h1 className={styles.title}>{text}</h1>;
-// }
-
 export default function SearchBar({ onSubmit }) {
   const [name, setName] = useState("");
 
